3686299983a363068bb27662e657cf6ede82d588
Silvio Giebl <gieblsilvio@outlook.com>
2025-04-21 17:27:41 +0200

    Return relative path in StructureProjectExtension if input was relative

--- src/main/kotlin/io/github/sgtsilvio/gradle/structure/StructureProjectExtension.kt
+++ src/main/kotlin/io/github/sgtsilvio/gradle/structure/StructureProjectExtension.kt
@@ -14,15 +14,20 @@ abstract class StructureProjectExtension @Inject internal constructor(
 ) {
 
     fun path(shortPath: String): String {
-        val absoluteShortPath = if (shortPath.startsWith(':')) {
+        val isAbsolute = shortPath.startsWith(':')
+        val absoluteShortPath = if (isAbsolute) {
             shortPath
         } else {
             val currentProjectShortPath = projectPathMapping.gradleToShort[project.path]
                 ?: throw IllegalStateException("$project was not defined by the structure plugin")
             currentProjectShortPath.resolveProjectPath(shortPath)
         }
-        return projectPathMapping.shortToFull[absoluteShortPath]
+        val fullPath = projectPathMapping.shortToFull[absoluteShortPath]
             ?: throw IllegalArgumentException("'$shortPath' is not a project path")
-        // TODO if was relative, make relative again
+        return if (isAbsolute) {
+            fullPath
+        } else {
+            fullPath.split(':').takeLast(shortPath.count { it == ':' } + 1).joinToString(":")
+        }
     }
 }
